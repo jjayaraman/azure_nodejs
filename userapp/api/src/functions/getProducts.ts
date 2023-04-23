8f8d861c08c8c678ebdd782b4e410a8737536c3f
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2023-04-23 12:37:56 +0100

    getPrducts added

---
+++ userapp/api/src/functions/getProducts.ts
@@ -0,0 +1,46 @@
+import { CosmosClient, SqlQuerySpec } from '@azure/cosmos';
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from '@azure/functions';
+
+export async function getProducts(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const connectionString = process.env.CosmosDbConnectionString;
+  const databaseId = process.env.COSMOS_DATABASE_ID;
+  const containerId = process.env.COSMOS_CONTAINER_ID;
+
+  context.log(`databaseId: ${databaseId}, containerId: ${containerId}`);
+
+  const client = new CosmosClient(connectionString);
+
+  // Select the database and container
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+
+  // Perform a query to get all items from the container
+  const querySpec: SqlQuerySpec = {
+    query: 'SELECT TOP 10 * FROM c',
+  };
+  const { resources } = await container.items.query(querySpec).fetchAll();
+
+  let response = {
+    body: JSON.stringify(resources),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  return response;
+}
+
+app.http('getProducts', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: getProducts,
+});
