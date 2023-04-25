60d84f815735a3e1b4add8da8337161009b902a3
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2023-04-25 18:51:08 +0100

    delete fix

--- jayfuncapp/products/api/src/functions/deleteProduct.ts
+++ jayfuncapp/products/api/src/functions/deleteProduct.ts
@@ -11,33 +11,39 @@ export async function deleteProduct(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+  let deleted: any;
+  try {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-  const connectionString = process.env.CosmosDbConnectionString;
-  const databaseId = process.env.COSMOS_DATABASE_ID;
-  const containerId = process.env.COSMOS_CONTAINER_ID;
+    const connectionString = process.env.CosmosDbConnectionString;
+    const databaseId = process.env.COSMOS_DATABASE_ID;
+    const containerId = process.env.COSMOS_CONTAINER_ID;
 
-  const client = new CosmosClient(connectionString);
-  const database = client.database(databaseId);
-  const container = database.container(containerId);
+    const client = new CosmosClient(connectionString);
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
 
-  const id = request.query.get('id') || request.params.id;
+    const id = request.query.get('id') || request.params.id;
 
-  const querySpec: SqlQuerySpec = {
-    query: 'SELECT * FROM c where c.id = @id',
-    parameters: [{ name: '@id', value: id }],
-  };
-  const { resources } = await container.items.query(querySpec).fetchAll();
+    const querySpec: SqlQuerySpec = {
+      query: 'SELECT * FROM c where c.id = @id',
+      parameters: [{ name: '@id', value: id }],
+    };
+    const { resources } = await container.items.query(querySpec).fetchAll();
 
-  context.log(`loaded : ${JSON.stringify(resources)}`);
+    context.log(`loaded : ${JSON.stringify(resources)}`);
 
-  const product = resources[0] as Product;
-  const deleted = await container.item(product.id, product.categoryId).delete();
+    const product = resources[0] as Product;
+    context.log(`product loaded : ${JSON.stringify(product)}`);
+    deleted = await container.item(product?.id, product?.categoryId).delete();
 
-  context.log(`deleted : ${JSON.stringify(deleted)}`);
+    context.log(`deleted : ${deleted}`);
+  } catch (error) {
+    context.log(`Error: ${error}`);
+  }
   return {
     status: 200,
-    body: JSON.stringify(product),
+    body: JSON.stringify(deleted),
     headers: {
       'content-type': 'application/json',
     },
