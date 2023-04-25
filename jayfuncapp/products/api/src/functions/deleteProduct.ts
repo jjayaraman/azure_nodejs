e9bce481cde6c106938303de3e50b70a9850e8b9
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2023-04-25 08:09:51 +0100

    deleted added

---
+++ jayfuncapp/products/api/src/functions/deleteProduct.ts
@@ -0,0 +1,49 @@
+import { CosmosClient, SqlQuerySpec } from '@azure/cosmos';
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from '@azure/functions';
+import Product from '../model/product';
+
+export async function deleteProduct(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const connectionString = process.env.CosmosDbConnectionString;
+  const databaseId = process.env.COSMOS_DATABASE_ID;
+  const containerId = process.env.COSMOS_CONTAINER_ID;
+
+  const client = new CosmosClient(connectionString);
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+
+  const id = request.query.get('id') || request.params.id;
+
+  const querySpec: SqlQuerySpec = {
+    query: 'SELECT * FROM c where c.id = @id',
+    parameters: [{ name: '@id', value: id }],
+  };
+  const { resources } = await container.items.query(querySpec).fetchAll();
+
+  const product = resources[0] as Product;
+  const deleted = await container.item(product.id, product.categoryId).delete();
+
+  context.log(`deleted : ${JSON.stringify(deleted)}`);
+  return {
+    status: 200,
+    body: JSON.stringify(product),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+}
+
+app.http('deleteProduct', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: deleteProduct,
+});
