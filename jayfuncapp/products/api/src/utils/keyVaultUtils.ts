7876e1e50060f6f6b0cb16749cae69518574e61b
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2023-05-01 07:32:26 +0100

    keyvaultutils added

---
+++ jayfuncapp/products/api/src/utils/keyVaultUtils.ts
@@ -0,0 +1,32 @@
+const { SecretClient } = require('@azure/keyvault-secrets')
+const { DefaultAzureCredential } = require('@azure/identity')
+
+const getClient = () => {
+  const keyVaultName = process.env.KEY_VAULT_NAME || 'jjaykeyvault'
+  const url = 'https://' + keyVaultName + '.vault.azure.net'
+  const credential = new DefaultAzureCredential()
+  const client = new SecretClient(url, credential)
+  return client
+}
+
+const createSecret = async (secretName: string, secretValue: string) => {
+  const client = await getClient()
+  const result = await client.setSecret(secretName, secretValue)
+  if (result) {
+    console.info(`Set secret ${secretName} created successfully`)
+  }
+}
+
+const getSecretValue = async (secretName: string) => {
+  // Read the secret we created
+  const client = await getClient()
+  const secret = await client.getSecret(secretName)
+  console.log(`Secret retrieved for secretName: ${secretName} `)
+  return secret
+}
+
+const deleteSecret = async (secretName: string) => {
+  const client = await getClient()
+  await client.beginDeleteSecret(secretName)
+  console.log(`Secret deleted for secretName: ${secretName}`)
+}
