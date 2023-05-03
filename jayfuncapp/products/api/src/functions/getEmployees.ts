b672973404ac034119765264e2c55319b3a242d9
Jayakumar Jayaraman <jjayaraman@tmhcc.com>
2023-05-03 11:23:20 +0100

    demo function added

---
+++ jayfuncapp/products/api/src/functions/getEmployees.ts
@@ -0,0 +1,24 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+export async function getEmployees(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get("name") || (await request.text()) || "world";
+
+  return { body: `Get employeesss, ${name}!` };
+}
+
+app.http("getEmployees", {
+  methods: ["GET"],
+  route: "employee/get",
+  authLevel: "anonymous",
+  handler: getEmployees,
+});
