6bd875d1de9038e28f0b394064e214299484fd3b
jay <india.jai@gmail.com>
2025-03-15 11:26:29 +0000

    github webhook code  added

--- githubwebhooksfunctionapp/src/functions/githubWebhookListener.ts
+++ githubwebhooksfunctionapp/src/functions/githubWebhookListener.ts
@@ -1,24 +1,5 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions'
-
-export async function githubWebhookListener(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-  console.log(`Received request cons log: ${JSON.stringify(request)}`)
-  context.log(`Received request ctx log: ${JSON.stringify(request)}`)
-  context.log(`Http function processed request for url "${request.url}"`)
-
-  request.headers.get('x-github-event')
-  request.headers.get('x-hub-signature')
-
-  const githubData = (await request.json()) || `No data received from GitHub`
-  context.log(`Received data from GitHub: ${JSON.stringify(githubData)}`)
-
-  const response = {
-    body: JSON.stringify(githubData),
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  }
-  context.log(`Sending response: ${JSON.stringify(response)}`)
-  return response
-}
+import { validateSignature } from '../utils/validationUtils'
 
 app.http('githubWebhookListener', {
   methods: ['POST'],
@@ -26,3 +7,49 @@ app.http('githubWebhookListener', {
   route: 'githubwebhooklistener',
   handler: githubWebhookListener,
 })
+
+export async function githubWebhookListener(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  try {
+    context.log(`Received request: ${JSON.stringify(request)}`)
+    const eventType = request.headers.get('X-GitHub-Event')
+    const secret = process.env.GHWHS
+
+    if (!secret) {
+      context.error('GitHub Webhook Secret is not set')
+      throw new Error('Internal Server Error')
+    }
+
+    const payload = await request.text()
+    const signature = request.headers.get('X-Hub-Signature-256')
+
+    // Compute the hash of the request body
+    const isValid = await validateSignature(payload, signature, secret, context)
+    if (!isValid) {
+      return {
+        body: JSON.stringify({ message: 'Unauthorized' }),
+        status: 401,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    }
+
+    const githubData = JSON.parse(payload) || `No data received from GitHub`
+    context.log(`Received data from GitHub: ${JSON.stringify(githubData)}`)
+    // Do the business logic with the data received from GitHub, as needed.
+
+    // Respond back to Gitub with a message to the webhook event
+    const response = {
+      body: JSON.stringify({ message: `Received ${eventType} event from GitHub successfully` }),
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    }
+    context.log(`Sending response back to github: ${JSON.stringify(response)}`)
+    return response
+  } catch (error) {
+    context.error(`Error occurred:: ${error}`)
+    return {
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    }
+  }
+}
---
+++ githubwebhooksfunctionapp/src/utils/validationUtils.ts
@@ -0,0 +1,28 @@
+import { HttpRequest, InvocationContext } from '@azure/functions'
+import { createHmac, timingSafeEqual } from 'crypto'
+
+/**
+ * Validate the request signature
+ *
+ * @param request
+ * @param context
+ * @param secret
+ * @returns
+ */
+export const validateSignature = async (payload: string, signature: string, secret: string, context: InvocationContext) => {
+  if (!signature || !secret || !payload || !signature?.startsWith('sha256=')) {
+    context.error('validateSignature() - Invalid inputs')
+    return false
+  }
+
+  const computedSignature = createHmac('sha256', secret).update(payload).digest('hex')
+  context.log(`Computed signature: ${computedSignature}`)
+
+  const expectedSignature = signature?.split('=')[1]
+  context.log(`Expected signature: ${expectedSignature}`)
+
+  const isValid = timingSafeEqual(Buffer.from(computedSignature, 'hex'), Buffer.from(expectedSignature, 'hex'))
+  context.log(`Is valid signature: ${isValid}`)
+
+  return isValid
+}
